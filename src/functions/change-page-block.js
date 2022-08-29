aac49bdc69f731d6055b5a69f1ed6ada8fcf2127
Julia Kupreichik <julilike@yandex.ru>
2022-08-29 09:59:47 +0300

    feat: build application

--- src/functions/change-page-block.js
+++
@@ -1,9 +0,0 @@
-export function changePageBlock(elem, elemNext) {
-  elem.classList.add('block-hiding');
-  window.setTimeout(function () {
-    elem.classList.add('block-hidden');
-    elem.classList.remove('block-hiding');
-    elemNext.classList.remove('block-hidden');
-    elemNext.classList.remove('block-hiding');
-  }, 500);
-}
--- src/index.js
+++
@@ -1,8 +0,0 @@
-const { block1, block2, deck } = require("./dom-elements");
-const { block1EventHandler } = require("./functions/block1-event-handler");
-const { block2EventHandler } = require("./functions/block2-event-handler");
-const { playCards } = require("./functions/play-kards");
-
-block1.addEventListener('click', block1EventHandler);
-block2.addEventListener('click', block2EventHandler);
-deck.addEventListener('click', playCards);
